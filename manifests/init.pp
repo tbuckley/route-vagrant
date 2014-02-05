f085a1d8ab4d96d85f8a837a0309f56423f7fa68
Tom Buckley <tbuckley89@gmail.com>
2014-02-05 11:33:55 -0800

    Added puppet modules

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+exec { "apt-get update" :
+  command => "/usr/bin/apt-get update",
+}
+
+# Install git
+package { "git" :
+  ensure => "installed",
+  require => Exec[ "apt-get update" ],
+}
+
+
+# Install nodejs (order matters!)
+class { "nodejs":
+  version => "stable",
+  require => Exec[ "apt-get update" ],
+}
+
+# Install node modules
+package { "socket.io" :
+  ensure => "installed",
+  provider => "npm",
+  require => Class[ "nodejs" ],
+}
